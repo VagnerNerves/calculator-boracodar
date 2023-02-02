9e5bdb3288fcc711beb90eca45f4c64ce5782296
Vagner Nerves <vagnernervessantos@gmail.com>
2023-02-02 17:07:23 -0300

    feat: Ajusted Theme.

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,1 +1,18 @@
-export default {}
+export default {
+  FONT_FAMILY: {
+    REGULAR: 'Rubik_400Regular'
+  },
+  FONT_SIZE: {
+    SIZE_20: 20,
+    SIZE_24: 24,
+    SIZE_36: 36
+  },
+  COLORS: {
+    GRAY_100: '#EBEBEB',
+    GRAY_600: '#6B6B6B',
+    GRAY_800: '#2D2A37',
+    VIOLET_500: '#975DFA',
+    VIOLET_600: '#7F45E2',
+    VIOLET_800: '#462878'
+  }
+}
