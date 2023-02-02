1196ebf34f4b864815486e44062341d506fbf674
Vagner Nerves <vagnernervessantos@gmail.com>
2023-02-02 16:49:45 -0300

    feat: Crated component Calculator.

---
+++ src/screens/Calculator/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+import { Container } from './styles'
+
+export function Calculator() {
+  return (
+    <Container>
+      <Text>Calculator</Text>
+    </Container>
+  )
+}
